a575cae1444215135101d417338737f9f2cead51
Lekan <jimoridwanlekan@gmail.com>
2022-07-28 14:53:45 +0100

    Add negative test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,6 +5,7 @@ describe Solver do
     solver = Solver.new
     expect(solver.factorial(5)).to eq(120)
     expect(solver.factorial(0)).to eq(1)
+    expect(solver.factorial(-1)).to raise_error('No negative factorial')
   end
 
   it 'returns a reversed string' do
